8a3de91ee927ef3bcc066713bdf091e9935ddc61
Terri Hounsom <hounsom.terri@gmail.com>
2019-08-16 12:46:35 +0100

    allowed for photo upload

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -36,7 +36,7 @@ class CocktailsController < ApplicationController
 
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 
 end
